a8da26798990b593c098e8345eff5b92a2fe3e66
ilysha <me@vasin-ilya.ru>
2017-01-20 19:43:21 +0700

    build fix

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -9,10 +9,6 @@ exports.config = {
   baseUrl: 'http://localhost:3000/',
 
   // use `npm run e2e`
-  specs: [
-    helpers.root('src/**/**.e2e.ts'),
-    helpers.root('src/**/*.e2e.ts')
-  ],
   exclude: [],
 
   framework: 'jasmine2',
